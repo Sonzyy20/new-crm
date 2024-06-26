8cc7ca3c572f24f4ec090a6428aecf0ad2fe19da
тимофей кабыш <timajaf2005@gmail.com>
2024-06-26 12:04:33 +0200

    little fixing

--- new-crm/components/kanban/slideover/SliderOver.vue
+++ new-crm/components/kanban/slideover/SliderOver.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { useDealSliderStore } from '~/store/deal-slide.store';
 const store = useDealSliderStore()
+
+const isLocalOpen = computed({
+    get: () => store.isOpen,
+    set: value => {
+        store.isOpen = value;
+    }
+})
+
+
 </script>
 
 <template>
